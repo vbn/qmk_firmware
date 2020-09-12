e895783761a93458b54972fb407d15d1a23e4e1f
Manna Harbour <51143715+manna-harbour@users.noreply.github.com>
2020-09-12 16:10:33 +1000

    Add layer toggle keycodes

--- keyboards/crkbd/keymaps/miryoku-examples-add-layers/config.h
+++ keyboards/crkbd/keymaps/miryoku-examples-add-layers/config.h
@@ -42,7 +42,7 @@
          N30,   N31,   K32,   K33,   K34,          K35,   K36,   K37,   N38,   N39\
 )\
 LAYOUT_split_3x6_3(\
-KC_LALT, K00,   K01,   K02,   K03,   K04,          K05,   K06,   K07,   K08,   K09,   KC_LALT,\
+KC_LALT, K00,   K01,   K02,   K03,   K04,          K05,   K06,   K07,   K08,   K09,   TG(GAME),\
 KC_LCTL, K10,   K11,   K12,   K13,   K14,          K15,   K16,   K17,   K18,   K19,   KC_LCTL,\
 KC_LSFT, K20,   K21,   K22,   K23,   K24,          K25,   K26,   K27,   K28,   K29,   KC_LSFT,\
                        K32,   K33,   K34,          K35,   K36,   K37\
--- keyboards/crkbd/keymaps/miryoku-examples-add-layers/keymap.c
+++ keyboards/crkbd/keymaps/miryoku-examples-add-layers/keymap.c
@@ -136,7 +136,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_NP,   KC_NP,   KC_LPRN, KC_RPRN, KC_UNDS, KC_NA,   KC_NA,   KC_NA,   KC_NP,   KC_NP
   ),
   [GAME] = LAYOUT_split_3x6_3(
-    KC_LALT, KC_Q,  KC_W,  KC_F,   KC_P,   KC_B,   KC_J,   KC_L,    KC_U,    KC_Y,   KC_QUOT, KC_LALT,
+    KC_LALT, KC_Q,  KC_W,  KC_F,   KC_P,   KC_B,   KC_J,   KC_L,    KC_U,    KC_Y,   KC_QUOT, TG(GAME),
     KC_LCTL, KC_A,  KC_R,  KC_S,   KC_T,   KC_G,   KC_M,   KC_N,    KC_E,    KC_I,   KC_O,    KC_LCTL,
     KC_LSFT, KC_Z,  KC_X,  KC_C,   KC_D,   KC_V,   KC_K,   KC_H,    KC_COMM, KC_DOT, KC_SLSH, KC_LSFT,
                            KC_ESC, KC_SPC, KC_TAB, KC_ENT, KC_BSPC, KC_DEL
