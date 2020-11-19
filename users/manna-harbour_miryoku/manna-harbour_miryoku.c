fe81c5acad4bbc0d62e2af2aa2273db9ebe01603
Manna Harbour <51143715+manna-harbour@users.noreply.github.com>
2020-11-19 14:23:03 +1100

    Add layer lock

--- users/manna-harbour_miryoku/manna-harbour_miryoku.c
+++ users/manna-harbour_miryoku/manna-harbour_miryoku.c
@@ -57,39 +57,39 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [NAVR] = LAYOUT_miryoku(
     RESET,   X_NA,    X_NA,    X_NA,    X_NA,    KC_AGIN, KC_UNDO, KC_CUT,  KC_COPY, KC_PSTE,
     KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, X_NA,    KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_CAPS,
-    X_NA,    KC_ALGR, X_NA,    X_NA,    X_NA,    KC_HOME, KC_PGDN, KC_PGUP, KC_END,  KC_INS,
-    X_NP,    X_NP,    X_NA,    X_NA,    X_NA,    KC_ENT,  KC_BSPC, KC_DEL,  X_NP,    X_NP
+    DF(NAVR),KC_ALGR, X_NA,    X_NA,    X_NA,    KC_HOME, KC_PGDN, KC_PGUP, KC_END,  KC_INS,
+    X_NP,    X_NP,    X_NA,    DF(BASE),X_NA,    KC_ENT,  KC_BSPC, KC_DEL,  X_NP,    X_NP
   ),
   [MOUR] = LAYOUT_miryoku(
     RESET,   X_NA,    X_NA,    X_NA,    X_NA,    X_NU,    X_NU,    X_NU,    X_NU,    X_NU,
     KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, X_NA,    KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, X_NU,
-    X_NA,    KC_ALGR, X_NA,    X_NA,    X_NA,    KC_WH_L, KC_WH_D, KC_WH_U, KC_WH_R, X_NU,
-    X_NP,    X_NP,    X_NA,    X_NA,    X_NA,    KC_BTN1, KC_BTN3, KC_BTN2, X_NP,    X_NP
+    DF(MOUR),KC_ALGR, X_NA,    X_NA,    X_NA,    KC_WH_L, KC_WH_D, KC_WH_U, KC_WH_R, X_NU,
+    X_NP,    X_NP,    X_NA,    X_NA,    DF(BASE),KC_BTN1, KC_BTN3, KC_BTN2, X_NP,    X_NP
   ),
   [MEDR] = LAYOUT_miryoku(
     RESET,   X_NA,    X_NA,    X_NA,    X_NA,    RGB_TOG, RGB_MOD, RGB_HUI, RGB_SAI, RGB_VAI,
     KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, X_NA,    KC_MPRV, KC_VOLD, KC_VOLU, KC_MNXT, X_NU,
-    X_NA,    KC_ALGR, X_NA,    X_NA,    X_NA,    X_NU,    X_NU,    X_NU,    X_NU,    X_NU,
-    X_NP,    X_NP,    X_NA,    X_NA,    X_NA,    KC_MSTP, KC_MPLY, KC_MUTE, X_NP,    X_NP
+    DF(MEDR),KC_ALGR, X_NA,    X_NA,    X_NA,    X_NU,    X_NU,    X_NU,    X_NU,    X_NU,
+    X_NP,    X_NP,    DF(BASE),X_NA,    X_NA,    KC_MSTP, KC_MPLY, KC_MUTE, X_NP,    X_NP
   ),
 #else
   [NAVR] = LAYOUT_miryoku(
     RESET,   X_NA,    X_NA,    X_NA,    X_NA,    KC_AGIN, KC_UNDO, KC_CUT,  KC_COPY, KC_PSTE,
     KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, X_NA,    KC_CAPS, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT,
-    X_NA,    KC_ALGR, X_NA,    X_NA,    X_NA,    KC_INS,  KC_HOME, KC_PGDN, KC_PGUP, KC_END,
-    X_NP,    X_NP,    X_NA,    X_NA,    X_NA,    KC_ENT,  KC_BSPC, KC_DEL,  X_NP,    X_NP
+    DF(NAVR),KC_ALGR, X_NA,    X_NA,    X_NA,    KC_INS,  KC_HOME, KC_PGDN, KC_PGUP, KC_END,
+    X_NP,    X_NP,    X_NA,    DF(BASE),X_NA,    KC_ENT,  KC_BSPC, KC_DEL,  X_NP,    X_NP
   ),
   [MOUR] = LAYOUT_miryoku(
     RESET,   X_NA,    X_NA,    X_NA,    X_NA,    X_NU,    X_NU,    X_NU,    X_NU,    X_NU,
     KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, X_NA,    X_NU,    KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R,
-    X_NA,    KC_ALGR, X_NA,    X_NA,    X_NA,    X_NU,    KC_WH_L, KC_WH_D, KC_WH_U, KC_WH_R,
-    X_NP,    X_NP,    X_NA,    X_NA,    X_NA,    KC_BTN1, KC_BTN3, KC_BTN2, X_NP,    X_NP
+    DF(MOUR),KC_ALGR, X_NA,    X_NA,    X_NA,    X_NU,    KC_WH_L, KC_WH_D, KC_WH_U, KC_WH_R,
+    X_NP,    X_NP,    X_NA,    X_NA,    DF(BASE),KC_BTN1, KC_BTN3, KC_BTN2, X_NP,    X_NP
   ),
   [MEDR] = LAYOUT_miryoku(
     RESET,   X_NA,    X_NA,    X_NA,    X_NA,    RGB_TOG, RGB_MOD, RGB_HUI, RGB_SAI, RGB_VAI,
     KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, X_NA,    X_NU,    KC_MPRV, KC_VOLD, KC_VOLU, KC_MNXT,
-    X_NA,    KC_ALGR, X_NA,    X_NA,    X_NA,    X_NU,    X_NU,    X_NU,    X_NU,    X_NU,
-    X_NP,    X_NP,    X_NA,    X_NA,    X_NA,    KC_MSTP, KC_MPLY, KC_MUTE, X_NP,    X_NP
+    DF(MEDR),KC_ALGR, X_NA,    X_NA,    X_NA,    X_NU,    X_NU,    X_NU,    X_NU,    X_NU,
+    X_NP,    X_NP,    DF(BASE),X_NA,    X_NA,    KC_MSTP, KC_MPLY, KC_MUTE, X_NP,    X_NP
   ),
 #endif
   [MBO] = LAYOUT_miryoku(
@@ -101,19 +101,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [FUNL] = LAYOUT_miryoku(
     KC_F12,  KC_F7,   KC_F8,   KC_F9,   KC_PSCR, X_NA,    X_NA,    X_NA,    X_NA,    RESET,
     KC_F11,  KC_F4,   KC_F5,   KC_F6,   KC_SLCK, X_NA,    KC_LSFT, KC_LCTL, KC_LALT, KC_LGUI,
-    KC_F10,  KC_F1,   KC_F2,   KC_F3,   KC_PAUS, X_NA,    X_NA,    X_NA,    KC_ALGR, X_NA,
-    X_NP,    X_NP,    KC_APP,  KC_SPC,  KC_TAB,  X_NA,    X_NA,    X_NA,    X_NP,    X_NP
+    KC_F10,  KC_F1,   KC_F2,   KC_F3,   KC_PAUS, X_NA,    X_NA,    X_NA,    KC_ALGR, DF(FUNL),
+    X_NP,    X_NP,    KC_APP,  KC_SPC,  KC_TAB,  X_NA,    X_NA,    DF(BASE),X_NP,    X_NP
   ),
   [NSL] = LAYOUT_miryoku(
     KC_LBRC, KC_7,    KC_8,    KC_9,    KC_RBRC, X_NA,    X_NA,    X_NA,    X_NA,    RESET,
     KC_SCLN, KC_4,    KC_5,    KC_6,    KC_EQL,  X_NA,    KC_LSFT, KC_LCTL, KC_LALT, KC_LGUI,
-    KC_GRV,  KC_1,    KC_2,    KC_3,    KC_BSLS, X_NA,    X_NA,    X_NA,    KC_ALGR, X_NA,
-    X_NP,    X_NP,    KC_DOT,  KC_0,    KC_MINS, X_NA,    X_NA,    X_NA,    X_NP,    X_NP
+    KC_GRV,  KC_1,    KC_2,    KC_3,    KC_BSLS, X_NA,    X_NA,    X_NA,    KC_ALGR, DF(NSL),
+    X_NP,    X_NP,    KC_DOT,  KC_0,    KC_MINS, X_NA,    DF(BASE),X_NA,    X_NP,    X_NP
   ),
   [NSSL] = LAYOUT_miryoku(
     KC_LCBR, KC_AMPR, KC_ASTR, KC_LPRN, KC_RCBR, X_NA,    X_NA,    X_NA,    X_NA,    RESET,
     KC_COLN, KC_DLR,  KC_PERC, KC_CIRC, KC_PLUS, X_NA,    KC_LSFT, KC_LCTL, KC_LALT, KC_LGUI,
-    KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_PIPE, X_NA,    X_NA,    X_NA,    KC_ALGR, X_NA,
-    X_NP,    X_NP,    KC_LPRN, KC_RPRN, KC_UNDS, X_NA,    X_NA,    X_NA,    X_NP,    X_NP
-  )
+    KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_PIPE, X_NA,    X_NA,    X_NA,    KC_ALGR, DF(NSSL),
+    X_NP,    X_NP,    KC_DOT,  KC_0,    KC_MINS, DF(BASE),X_NA,    X_NA,    X_NP,    X_NP
+  ),
 };
