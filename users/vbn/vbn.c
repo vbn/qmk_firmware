6f2643e410aa3529b4f3516b468c1063e992c608
Vaibhav Nagarnaik <vnagarnaik@gmail.com>
2022-03-30 23:01:55 -0700

    vbn: remove old code

--- users/vbn/vbn.c
+++ users/vbn/vbn.c
@@ -8,7 +8,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     U_NP,              U_NP,              U_NA,              LT(NAV, KC_SPC),   LT(MOUSE, KC_TAB), LT(FUN, KC_ENT),   LT(NUM, KC_BSPC),  U_NA,              U_NP,              U_NP
   ),
  [NAV] = LAYOUT_miryoku(
-    RESET,             U_NA,              U_NA,              U_NA,              U_NA,              U_NA,              U_NA,              U_NA,              U_NA,              U_NA,
+    U_NA,              U_NA,              U_NA,              U_NA,              U_NA,              U_NA,              U_NA,              U_NA,              U_NA,              U_NA,
     KC_LGUI,           KC_LALT,           KC_LCTL,           KC_LSFT,           U_NA,              KC_LEFT,           KC_DOWN,           KC_UP,             KC_RGHT,           KC_DEL,
     U_NA,              U_NA,              U_NA,              U_NA,              U_NA,              KC_HOME,           KC_PGDN,           KC_PGUP,           KC_END,            KC_INS,
     U_NP,              U_NP,              U_NA,              U_NA,              U_NA,              KC_ENT,            KC_BSPC,           U_NA,              U_NP,              U_NP
@@ -24,16 +24,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     LGUI_T(KC_6),      LALT_T(KC_7),      LCTL_T(KC_8),      LSFT_T(KC_9),      KC_0,              U_NA,              KC_LSFT,           KC_LCTL,           KC_LALT,           KC_LGUI,
     KC_BSLS,           KC_DOT,            KC_SCLN,           KC_LBRC,           KC_RBRC,           U_NA,              U_NA,              U_NA,              U_NA,              U_NA,
     U_NP,              U_NP,              U_NA,              KC_SPC,            KC_ESC,            U_NA,              U_NA,              U_NA,              U_NP,              U_NP
-    #if 0
-    /* alt */
-    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
-    KC_LGUI, LALT_T(KC_LBRC), LCTL_T(KC_RBRC), LSFT_T(KC_SCLN), KC_EQL, KC_MINS, LSFT_T(KC_GRV), LCTL_T(KC_BSLS), KC_LALT, KC_LGUI,
-    /* old */
-    KC_LBRC, KC_7,    KC_8,    KC_9,    KC_RBRC, U_NA,    U_NA,    U_NA,    U_NA,    RESET,
-    KC_SCLN, KC_4,    KC_5,    KC_6,    KC_EQL,  U_NA,    KC_LSFT, KC_LCTL, KC_LALT, KC_LGUI,
-    KC_GRV,  KC_1,    KC_2,    KC_3,    KC_BSLS, U_NA,    U_NA,    U_NA,    KC_ALGR, U_NA,
-    U_NP,    U_NP,    KC_DOT,  KC_0,    KC_MINS, U_NA,    U_NA,    U_NA,    U_NP,    U_NP
-    #endif
   ),
   [FUN] = LAYOUT_miryoku(
     KC_F1,             KC_F2,             KC_F3,             KC_F4,             KC_F5,            U_NA,              U_NA,              U_NA,              U_NA,               U_NA,
