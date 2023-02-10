cd8cb2fd691ed556dfea5cda4155a5fafaa69818
Vaibhav Nagarnaik <vnagarnaik@gmail.com>
2023-02-10 10:20:54 -0800

    vbn: switch media keys around

--- users/vbn/vbn.c
+++ users/vbn/vbn.c
@@ -14,9 +14,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     U_NP,              U_NP,              U_NA,              U_NA,              U_NA,              KC_ENT,            KC_BSPC,           U_NA,              U_NP,              U_NP
   ),
   [MOUSE] = LAYOUT_miryoku(
-    RESET,             U_NA,              U_NA,              U_NA,              U_NA,              KC_MPRV,           KC_VOLD,           KC_VOLU,           KC_MNXT,           KC_MPLY,
+    RESET,             U_NA,              U_NA,              U_NA,              U_NA,              KC_MPLY,           KC_VOLD,           KC_VOLU,           KC_MUTE,           KC_MPRV,
     KC_LGUI,           KC_LALT,           KC_LCTL,           KC_LSFT,           U_NA,              KC_MS_L,           KC_MS_D,           KC_MS_U,           KC_MS_R,           KC_BTN2,
-    U_NA,              KC_ALGR,           U_NA,              U_NA,              U_NA,              KC_WH_L,           KC_WH_D,           KC_WH_U,           KC_WH_R,           KC_MUTE,
+    U_NA,              KC_ALGR,           U_NA,              U_NA,              U_NA,              KC_WH_L,           KC_WH_D,           KC_WH_U,           KC_WH_R,           KC_MNXT,
     U_NP,              U_NP,              U_NA,              U_NA,              U_NA,              KC_BTN1,           KC_BTN3,           U_NA,              U_NP,              U_NP
   ),
   [NUM] = LAYOUT_miryoku(
